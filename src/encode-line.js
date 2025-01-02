2d947aa3cdee6051b58674dcbda4154492656a68
Everagi <belorusskiychuvak@gmail.com>
2025-01-02 17:32:49 +0300

    docs:solve sum and line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  return str
+    .split("")
+    .reduce((acc, curr) => {
+      let lastElement = acc[acc.length - 1];
+
+      if (lastElement && lastElement.character === curr) {
+        lastElement.count++;
+      } else {
+        acc.push({ character: curr, count: 1 });
+      }
+
+      return acc;
+    }, [])
+    .map(({ character, count }) =>
+      count > 1 ? `${count}${character}` : character
+    )
+    .join("");
 }
 
 module.exports = {
