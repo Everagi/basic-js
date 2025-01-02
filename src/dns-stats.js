ac4cb2c153ec7fac03b3eb52d53181960e51cdf3
Everagi <belorusskiychuvak@gmail.com>
2025-01-02 21:30:18 +0300

    docs: solve task dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let dnsStats = {};
+  for (let domain of domains) {
+    let subdomains = domain.split(".").reverse();
+    let dns = "";
+    for (let subdomain of subdomains) {
+      dns += "." + subdomain;
+      dnsStats[dns] = (dnsStats[dns] || 0) + 1;
+    }
+  }
+  return dnsStats;
 }
 
 module.exports = {
