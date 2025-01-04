01ac7f7b4fd2e435ccb1a34e3ae9fb6098670906
Everagi <belorusskiychuvak@gmail.com>
2025-01-04 09:48:09 +0300

    docs: solve task vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error("Incorrect arguments!");
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = "";
+    let keyIndex = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i])) {
+        let shift =
+          (this.alphabet.indexOf(message[i]) +
+            this.alphabet.indexOf(key[keyIndex % key.length])) %
+          this.alphabet.length;
+        result += this.alphabet[shift];
+        keyIndex++;
+      } else {
+        result += message[i];
+      }
+    }
+    return this.direct ? result : result.split("").reverse().join("");
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error("Incorrect arguments!");
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    let result = "";
+    let keyIndex = 0;
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (this.alphabet.includes(encryptedMessage[i])) {
+        let shift =
+          (this.alphabet.indexOf(encryptedMessage[i]) +
+            this.alphabet.length -
+            this.alphabet.indexOf(key[keyIndex % key.length])) %
+          this.alphabet.length;
+        result += this.alphabet[shift];
+        keyIndex++;
+      } else {
+        result += encryptedMessage[i];
+      }
+    }
+    return this.direct ? result : result.split("").reverse().join("");
   }
 }
 
